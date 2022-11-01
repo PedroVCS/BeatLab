733fb7322c27cd3571ef12217a6483b539b4164a
PedroVCS <pedro_victor61@hotmail.com>
2022-11-01 15:00:43 -0300

    att css buttons

---
+++ js/finalizarcompra.js
@@ -0,0 +1,8 @@
+
+// var button = document.getElementById("action-bt");
+
+// button.addEventListener("click", function(){
+//     var dados = document.getElementById("dados");
+
+//     dados.classList.toggle("hide");
+// });
