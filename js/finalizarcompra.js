8718e2f702310020a2df238e9d25cb9bdcb03214
PedroVCS <pedro_victor61@hotmail.com>
2022-11-03 21:12:52 -0300

    Add js

--- js/finalizarcompra.js
+++ js/finalizarcompra.js
@@ -1,4 +1,35 @@
 
+var button = document.getElementById("action-bt");
+
+button.addEventListener("click", function(){
+    var dados = document.getElementById("dados");
+
+    dados.classList.toggle("hide");
+});
+
+// function finalizar(){
+//     const modal = document.getElementById('janela-modal')
+//     modal.classList.add('abrir')
+
+//     modal.addEventListener('click', (e) => {
+//         if(e.target.id == 'home-bt' || e.target.id == 'janela-modal'){
+//             modal.classList.remove('abrir')
+//         }         
+//     })
+// };
+
+
+function finalizar(){
+    const modal = document.getElementById('janela-modal')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'home-bt'){
+            modal.classList.remove('abrir')
+        }         
+    })
+};
+
 // var button = document.getElementById("action-bt");
 
 // button.addEventListener("click", function(){
@@ -6,3 +37,4 @@
 
 //     dados.classList.toggle("hide");
 // });
+
