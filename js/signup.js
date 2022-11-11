3379aeb68691c0857d2d861a8b5a21737e09a366
PedroVCS <pedro_victor61@hotmail.com>
2022-11-11 01:24:44 -0300

    add formulario signup

---
+++ js/signup.js
@@ -0,0 +1,56 @@
+const infCadastro = document.querySelector('#formUp');
+
+infCadastro.onsubmit = evento => {
+    //receber o valor do campo 
+    var nome = document.querySelector('#name').value;
+    //verificar se o campo esta em branco 
+    if(nome === "") {
+    evento.preventDefault();
+    document.getElementById('msgAlerta').innerHTML = '<p> Humm Algo esta de errado?</p><p> Campo Nome é obrigatorio!</p>'
+    return;
+}
+    //receber o valor do campo 
+    var email = document.querySelector('#email').value;
+    //verificar se o campo esta em branco 
+    if(email === "") {
+    evento.preventDefault();
+    document.getElementById('msgAlerta').innerHTML = '<p> Humm Algo esta de errado?</p><p>Campo Email é obrigatorio!</p>'
+    return;
+}
+    //receber o valor do campo 
+    var repetEmail = document.querySelector('#repetEmail').value;
+    //verificar se o campo esta em branco 
+    if(repetEmail === '') {
+    evento.preventDefault();
+    document.getElementById('msgAlerta').innerHTML = '<p> Humm Algo esta de errado?</p><p>Você precisa confirmar seu email!</p>'
+    return;
+}
+    //receber o valor do campo 
+    var password = document.querySelector('#password').value;
+    //verificar se o campo esta em branco 
+    if(password === '') {
+    evento.preventDefault();
+    document.getElementById('msgAlerta').innerHTML = '<p> Humm Algo esta de errado?</p><p>Você Não pode esquecer da sua senha!</p>'
+    return;
+}
+ //receber o valor do campo 
+ var repetPassword = document.querySelector('#repetPassword').value;
+ //verificar se o campo esta em branco 
+ if(repetPassword === '') {
+ evento.preventDefault();
+ document.getElementById('msgAlerta').innerHTML = '<p> Humm Algo esta de errado?</p><p>Você precisa repetir sua senha!</p>'
+ return;
+}
+};
+
+//JANELA MODAL PARA VOLTAR AO HOME APOS FINALIZAR O CADASTRO
+function finalizar(msgAlerta){
+    const modal = document.getElementById('janela-modal')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'home-bt'){
+            modal.classList.remove('abrir')
+        }         
+    })
+};
