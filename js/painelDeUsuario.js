53f56d0f30192922a63bebc4f5756b09d583ecd1
PedroVCS <pedro_victor61@hotmail.com>
2022-11-10 22:26:47 -0300

    delete pg perfil

--- js/painelDeUsuario.js
+++
@@ -1,8 +0,0 @@
-//FUNCTION PARA A DIV DE FORMA DE PAGAMNTO FICAR NONE
-function Mudarestado(el) {
-    var display = document.getElementById(el).style.display;
-    if (display == "none")
-      document.getElementById(el).style.display = 'block';
-    else
-      document.getElementById(el).style.display = 'none';
-  }
