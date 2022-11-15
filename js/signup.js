f8b94f84f5d04a75b95bc191dcb8ea402b666968
PedroVCS <pedro_victor61@hotmail.com>
2022-11-14 22:17:48 -0300

    att js

--- js/signup.js
+++ js/signup.js
@@ -2,11 +2,15 @@ const infCadastro = document.querySelector('#formUp');
 
 infCadastro.onsubmit = evento => {
     //receber o valor do campo 
-    var nome = document.querySelector('#name').value;
+    const nome = document.querySelector('#name');
+    let cor1 = document.querySelector('#name1');
+
     //verificar se o campo esta em branco 
-    if(nome === "") {
-    evento.preventDefault();
-    document.getElementById('msgAlerta').innerHTML = '<p> Humm Algo esta de errado?</p><p> Campo Nome é obrigatorio!</p>'
+    if(!nome.value) {
+   evento.preventDefault();
+    document.getElementById('msgAlerta').innerHTML = '<p> Hum...Algo deu errado.</p><p> Campo Nome é obrigatorio!</p>'
+     cor1.style.color = '#FF0000';
+    console.log(nome);
     return;
 }
     //receber o valor do campo 
@@ -14,7 +18,7 @@ infCadastro.onsubmit = evento => {
     //verificar se o campo esta em branco 
     if(email === "") {
     evento.preventDefault();
-    document.getElementById('msgAlerta').innerHTML = '<p> Humm Algo esta de errado?</p><p>Campo Email é obrigatorio!</p>'
+    document.getElementById('msgAlerta').innerHTML = '<p> Hum...Algo deu errado.</p><p>Campo Email é obrigatorio!</p>'
     return;
 }
     //receber o valor do campo 
@@ -22,7 +26,7 @@ infCadastro.onsubmit = evento => {
     //verificar se o campo esta em branco 
     if(repetEmail === '') {
     evento.preventDefault();
-    document.getElementById('msgAlerta').innerHTML = '<p> Humm Algo esta de errado?</p><p>Você precisa confirmar seu email!</p>'
+    document.getElementById('msgAlerta').innerHTML = '<p> Hum...Algo deu errado.</p><p>Você precisa confirmar seu email!</p>'
     return;
 }
     //receber o valor do campo 
@@ -30,7 +34,7 @@ infCadastro.onsubmit = evento => {
     //verificar se o campo esta em branco 
     if(password === '') {
     evento.preventDefault();
-    document.getElementById('msgAlerta').innerHTML = '<p> Humm Algo esta de errado?</p><p>Você Não pode esquecer da sua senha!</p>'
+    document.getElementById('msgAlerta').innerHTML = '<p> Hum...Algo deu errado.</p><p>Você Não pode esquecer da sua senha!</p>'
     return;
 }
  //receber o valor do campo 
@@ -38,18 +42,19 @@ infCadastro.onsubmit = evento => {
  //verificar se o campo esta em branco 
  if(repetPassword === '') {
  evento.preventDefault();
- document.getElementById('msgAlerta').innerHTML = '<p> Humm Algo esta de errado?</p><p>Você precisa repetir sua senha!</p>'
+ document.getElementById('msgAlerta').innerHTML = '<p> Hum...Algo deu errado?</p><p>Você precisa repetir sua senha!</p>'
  return;
 }
+
 };
 
 //JANELA MODAL PARA VOLTAR AO HOME APOS FINALIZAR O CADASTRO
-function finalizar(msgAlerta){
+function finalizar(){
     const modal = document.getElementById('janela-modal')
     modal.classList.add('abrir')
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'home-bt'){
+        if(e.target.id == 'home-bt' ){
             modal.classList.remove('abrir')
         }         
     })
