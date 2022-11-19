2934aa6d5f020f44209f8f825762ba58c33dbec6
PedroVCS <pedro_victor61@hotmail.com>
2022-11-18 23:01:46 -0300

    add menu Hamburguer

--- js/home.js
+++ js/home.js
@@ -6,10 +6,6 @@ document.getElementById("carrosselScroll1").checked = true;
 setInterval(function(){
     nextImage();
 }, 5000);
-
-
-
-
 //função carossel 
 function nextImage(){
     count++;
@@ -18,3 +14,24 @@ function nextImage(){
     }
     document.getElementById("radio"+count).checked = true;
 }
+ //menu hamburguer
+const btnMobile = document.getElementById("btn-mobile")
+
+function toggleMenu(event){
+    if(event.type==="touchstart"){
+        event.preventDefault()
+    }
+    
+    const nav = document.getElementById("nav")
+    nav.classList.toggle("active")
+    const active=nav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expanded',active)
+    if(active){ 
+        event.currentTarget.setAttribute('aria-label','Fechar Menu')
+    }else{
+        event.currentTarget.setAttribute('aria-label','Abrir Menu')
+    }
+}
+
+btnMobile.addEventListener("click", toggleMenu)
+btnMobile.addEventListener("touchstart", toggleMenu)
